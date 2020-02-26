1b80f2555670558f0b3fea64ae4e893be634823a
tatjama <tanja120a@gmail.com>
2020-02-26 08:52:15 +0100

    on Form Submit, evant prevent default function, to prevent automatic sending information to the backend

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,15 +3,17 @@ import React from 'react';
 class SearchBar extends React.Component{
     
 
-    onInputChange = (event) =>{
-        
+    onInputChange = (event) =>{        
         console.log(event.target.value);
+    }
+    onFormSubmit = (event)=>{
+        event.preventDefault();
     }
 
     render(){
     return(
         <div className = "ui segment">
-            <form className = "ui form">
+            <form className = "ui form" onSubmit = {this.onFormSubmit}>
                 <div className = "field">
                     <label>Search Image</label>
                     <input type ="text" onChange = {this.onInputChange}/>
