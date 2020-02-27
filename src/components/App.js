978d4f1a02a57c9361285675839798e35d131888
tatjama <tanja120a@gmail.com>
2020-02-27 10:38:39 +0100

    We take response and render number of images on the screen

--- src/components/App.js
+++ src/components/App.js
@@ -4,22 +4,25 @@ import SearchBar from './SearchBar';
 import ImageList from './ImageList';
 
 class App extends React.Component{
-    
+    state = {images: 0}
  onSearchSubmit = async (term)=>{
 //console.log(term);
 const response   = await unsplash.get('/search/photos',{
-        params: {query : term},
-        
+        params: {query : term},        
     });
-    console.log(response.data.results);
+    console.log(response.data.total);
+    this.setState({images: response.data.total});
+    console.log(this.state.images);
+
 }
     
     render(){
+        console.log(this)
     return(
         <div style = {{marginTop: "10px"}} className = "ui container">
         
             <SearchBar onSubmit = {this.onSearchSubmit} />
-            Found images
+            Found {this.state.images} images
             <ImageList/>
         </div>
     );
